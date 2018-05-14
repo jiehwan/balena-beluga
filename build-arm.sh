c5a143eb5e3613a6cd024a84b173caaef69947a2
jiehwan <jiehwan.park@samsung.com>
2018-05-15 01:08:13 +0900

    add for arm cross compile

---
+++ build-arm.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+set -o errexit
+
+case "$(go env GOARCH)" in
+	"arm")
+		arch="armv$(go env GOARM)"
+		;;
+	"arm64")
+		arch="aarch64"
+		;;
+	"386")
+		arch="i386"
+		;;
+	"amd64")
+		arch="x86_64"
+		;;
+esac
+
+version=$(git describe --tags --always)
+
+AUTO_GOPATH=1 \
+CC=arm-linux-gnueabi-gcc GOOS=linux GOARCH=arm GOARM=7 CGO_ENABLED=1 \
+DOCKER_BUILDTAGS='exclude_graphdriver_btrfs exclude_graphdriver_devicemapper' \
+./hack/make.sh binary-balena
+
+src="bundles/latest/binary-balena"
+dst="balena"
+
+rm -rf "$dst"
+mkdir "$dst"
+
+cp -L "$src/balena" "$dst/balena"
+strip "$dst/balena"
+
+ln -s balena "$dst/balenad"
+ln -s balena "$dst/balena-containerd"
+ln -s balena "$dst/balena-containerd-ctr"
+ln -s balena "$dst/balena-containerd-shim"
+ln -s balena "$dst/balena-proxy"
+ln -s balena "$dst/balena-runc"
+
+tar czfv "balena-$version-$arch.tar.gz" "$dst"
